5e4fba4b196eba342373151951389fc57199a750
Sagar Kumar <krsk987@gmail.com>
2024-05-10 00:54:12 +0530

    swagger

--- src/main/java/com/sagar/bankingapp/BankingAppApplication.java
+++ src/main/java/com/sagar/bankingapp/BankingAppApplication.java
@@ -1,9 +1,35 @@
 package com.sagar.bankingapp;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Banking Application",
+				description = "Backend Rest APIs for SK Bank",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Sagar Kumar",
+						email = "krsk987@gmail.com",
+						url = "https://github.com/sagrkr/Banking-App"
+				),
+				license = @License(
+						name = "Sagar Kumar",
+						url = "https://github.com/sagrkr/Banking-App"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Sagar Kumar Bank App Documentation",
+				url = "https://github.com/sagrkr/Banking-App"
+		)
+)
 public class BankingAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/sagar/bankingapp/Controllers/UserController.java
+++ src/main/java/com/sagar/bankingapp/Controllers/UserController.java
@@ -2,21 +2,39 @@ package com.sagar.bankingapp.Controllers;
 
 import com.sagar.bankingapp.Dtos.*;
 import com.sagar.bankingapp.Services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/api/user")
+@Tag(name = "User Account Management APIs")
 public class UserController {
     @Autowired
     UserService userService;
-
+    @Operation(
+            summary = "Create New User Account",
+            description = "Creating a new user assigning an account ID"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     @PostMapping()
     public BankResponse createAccount(@RequestBody UserRequest userRequest){
         return userService.createAccount(userRequest);
     }
-
+    @Operation(
+            summary = "Balance Enquiry",
+            description = "Given an account number, User can check account's current balance"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @GetMapping("balanceEnquiry")
     public BankResponse balanceEnquiry(@RequestBody EnquiryRequest request){
         return userService.balanceEnquiry(request);
